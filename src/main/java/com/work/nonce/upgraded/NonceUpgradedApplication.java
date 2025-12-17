08506631e439f10264608c64acb66b859a78c37f
deer-jay <h117d416@gmail.com>
2025-12-17 14:22:11 +0800

    nonce 升级版

---
+++ src/main/java/com/work/nonce/upgraded/NonceUpgradedApplication.java
@@ -0,0 +1,13 @@
+package com.work.nonce.upgraded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonceUpgradedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonceUpgradedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/nonce/upgraded/NonceUpgradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.nonce.upgraded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonceUpgradedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
